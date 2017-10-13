2ba941e28d9206f2cdad4ebc8f0ad5d2ed0df68c
Miran Karim <32594520+MiranKm@users.noreply.github.com>
2017-10-13 21:19:25 +0300

    EditText , bet numbers added not finished yet.

--- app/src/main/java/com/example/geraltofrivia/classagame/MainActivity.java
+++ app/src/main/java/com/example/geraltofrivia/classagame/MainActivity.java
@@ -3,18 +3,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.TicTaeTo.Mohammed;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnMiran2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnMiran2 = (Button) findViewById(R.id.btnMiran2);
     }
 
     //////////////////////////////////////////////////////
--- app/src/main/java/com/example/geraltofrivia/classagame/MiransActivity.java
+++ app/src/main/java/com/example/geraltofrivia/classagame/MiransActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,16 +15,26 @@ public class MiransActivity extends AppCompatActivity {
     Button randBtn;
     TextView randNumberTxt;
     TextView randNumberTxt2;
+    TextView betWon;
+    EditText getBetN;
     Random randNum1 = new Random();
+    String betNumber;
+
+    int randomNumber1;
+    int randomNumber2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mirans);
         randNumberTxt = (TextView) findViewById(R.id.randNumberTxt);
         randNumberTxt2 = (TextView) findViewById(R.id.randNumberTxt2);
+        betWon = (TextView) findViewById(R.id.betWon);
         randBtn =(Button) findViewById(R.id.randBtn);
+        getBetN =(EditText) findViewById(R.id.getBetN);
         randBtn.setOnClickListener(generate);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -34,11 +45,23 @@ public class MiransActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.randBtn:
-                    int randomNumber1 = (randNum1.nextInt(6) +1 ) ;
-                    int randomNumber2 = (randNum1.nextInt(6) +1 ) ;
+                     randomNumber1 = (randNum1.nextInt(6) +1 ) ;
+                     randomNumber2 = (randNum1.nextInt(6) +1 ) ;
                     randNumberTxt.setText(String.valueOf(randomNumber1));
                     randNumberTxt2.setText(String.valueOf(randomNumber2));
 
+                    betNumber =getBetN.getText().toString();
+
+                    int betNumberInteger = Integer.parseInt(betNumber);
+
+                    int number1 = betNumberInteger + 10;
+                    int number2= betNumberInteger - 10 ;
+                    if(randomNumber1 > randomNumber2){
+                        betWon.setText("" +number1);
+                        break;
+                    }else {
+                        betWon.setText( "" + number2);
+                    }
             }
         }
     };
@@ -52,5 +75,7 @@ public class MiransActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
 //////////////////////////////////////////////////////////////////////
