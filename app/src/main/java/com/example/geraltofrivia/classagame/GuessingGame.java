672621c84935ed310c4996a569a55af487a0a4ec
Rebin Muhammed <rebin.mahmed@gmail.com>
2017-10-14 13:25:38 +0300

    Addy My Activity & Card view

---
+++ app/src/main/java/com/example/geraltofrivia/classagame/GuessingGame.java
@@ -0,0 +1,13 @@
+package com.example.geraltofrivia.classagame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessingGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guessing_game);
+    }
+}
